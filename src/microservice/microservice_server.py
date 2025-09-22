1b4e96866a3c368d1f8b7b776f8e3e86b55aae65
Jack Sprott <jacksprott@proton.me>
2025-09-22 14:14:07 +0200

    Removed dictionary input

--- src/microservice/microservice_server.py
+++ src/microservice/microservice_server.py
@@ -2,21 +2,19 @@ from flask import Flask, request, abort
 import threading
 
 class MicroserviceServer:
-    def __init__(self, target, config):
+    def __init__(self, target, restrict_network_partner, allowed_network_partners, route="/", flask_listen_ip="0.0.0.0", server_port=5000):
         self.target = target
-        self.config = config
-        filter_ip = config["restrict_network_partner"]
         self.flask_app = Flask(__name__)
-        @self.flask_app.route(config["route"], methods=['POST'])
+        @self.flask_app.route(route, methods=['POST'])
         def flask_receive_data():
             addr = request.remote_addr
-            if filter_ip and not addr in config["allowed_network_partners"]:
+            if restrict_network_partner and not addr in allowed_network_partners:
                 abort(403)
             r_json = request.json
             result = getattr(target, r_json["function"])(*r_json["args"],**r_json["kwargs"])
             return result
         self.flask_thread = threading.Thread(target=self.flask_app.run, 
-                                            kwargs={"host":self.config["flask_listen_ip"], "port":self.config["server_port"]})
+                                            kwargs={"host":flask_listen_ip, "port":server_port})
         self.flask_thread.start()
         print("Microservice started")
     def join(self):
